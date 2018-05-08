e816d95c79f112aa9b63eb59e43c1a01bc26a8ac
Kostiantyn4 <kostyantun4@hotmail.com>
2018-05-09 00:43:56 +0300

    Upgaded Angular and other libs
    Upgraded Angular to version 6 with other libs

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -28,7 +28,6 @@ namespace DatingApp.API
             services.AddDbContext<DataContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default"))); 
             services.AddMvc();
             services.AddCors();
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.1
