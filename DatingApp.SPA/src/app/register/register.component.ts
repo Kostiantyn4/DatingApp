47594e6e08ca9371f05bde132c248556431d03fd
Kostiantyn4 <kostyantun4@hotmail.com>
2018-09-22 20:29:23 +0300

    Bootstrap, Angular etc. lib's  updated

--- DatingApp.SPA/src/app/register/register.component.ts
+++ DatingApp.SPA/src/app/register/register.component.ts
@@ -4,7 +4,7 @@ import { AuthService } from '../sevices/auth.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
 
